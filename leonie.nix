dac5a0a3481cce8ea577b5beaca0f8ad2f492f90
Kilian Mayer <mayer-kilian@gmx.de>
2025-11-29 10:45:56 +0100

    flake based setup, catppuccin theme installed

---
+++ leonie.nix
@@ -0,0 +1,16 @@
+{ ... }:
+{
+  imports = [
+    ./modules/programs.nix
+  ];
+  nixpkgs.config.allowUnfree = true;
+
+  home.username = "leonie";
+  home.homeDirectory = "/home/leonie";
+  home.stateVersion = "25.05";
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+
+  programs.home-manager.enable = true;
+}
