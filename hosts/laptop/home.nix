3e25d03cb14d283eb8c999e9d37d7d928eec39ff
Kilian Mayer <mayer-kilian@gmx.de>
2025-12-13 15:40:51 +0100

    resolved scaling issues

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -10,6 +10,10 @@
   home.username = "user";
   home.homeDirectory = "/home/user";
 
+  home.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
+
   stylix = {
     targets = {
       zen-browser.profileNames = [ "Default Profile" ];
