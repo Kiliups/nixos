9f1f8024af75b66caa49af54d68eb2ca350851de
Kilian Mayer <mayer-kilian@gmx.de>
2026-07-12 11:51:01 +0200

    sleep functionality for niri

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -1,7 +1,38 @@
-_: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
   imports = [
     ../home.nix
     ../../modules/linux/plasma.nix
     ../../modules/linux/tiling
   ];
+
+  services.swayidle = {
+    enable = true;
+    events.before-sleep = "${lib.getExe config.programs.noctalia-shell.package} ipc call lockScreen lock";
+    timeouts = [
+      {
+        timeout = 900;
+        command = "${lib.getExe config.programs.noctalia-shell.package} ipc call lockScreen lock";
+      }
+      {
+        timeout = 1800;
+        command = "${lib.getExe pkgs.niri} msg action power-off-monitors";
+        resumeCommand = "${lib.getExe pkgs.niri} msg action power-on-monitors";
+      }
+      {
+        timeout = 3600;
+        command = "${lib.getExe' pkgs.systemd "systemctl"} suspend";
+      }
+      {
+        timeout = 14400;
+        command = "${lib.getExe' pkgs.systemd "systemctl"} poweroff";
+      }
+    ];
+  };
+
 }
